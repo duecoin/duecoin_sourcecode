a7b1cfe7088250c9f4622885f81f1965953f927b
Duecoin <32307718+duecoin@users.noreply.github.com>
2017-10-03 14:00:58 -0700

    Re-upload

--- README.md
+++
@@ -1,2 +0,0 @@
-RPC Port: 25520
-Network Port: 25524
--- share/qt/make_windows_icon.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# create multiresolution windows icon
-ICON_DST=../../src/qt/res/icons/DueCoin.ico
-
-convert ../../src/qt/res/icons/DueCoin-16.png ../../src/qt/res/icons/DueCoin-32.png ../../src/qt/res/icons/DueCoin-48.png ${ICON_DST}
